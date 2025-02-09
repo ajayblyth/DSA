c7084749937856bfc09bb19053915b9cf5f7a022
ajayblyth <ajayblyth@gmail.com>
2025-02-09 19:58:03 +0530

    array

---
+++ LinkedList/src/array/EvenOdd.java
@@ -0,0 +1,26 @@
+package array;
+
+import java.util.ArrayList;
+
+public class EvenOdd {
+    public static void main(String[] args) {
+        int[] arr = { 12,34,56,44,77,89,75};
+        evenOdd(arr);
+
+    }
+    public static void evenOdd(int[] arr){
+ArrayList <Integer> evenNumber = new ArrayList<>();
+ArrayList<Integer > oddNumber = new ArrayList<>();
+
+for(int c :arr){
+    if(c % 2 == 0){
+        evenNumber.add(c);
+    }else{
+        oddNumber.add(c);
+    }
+
+}
+        System.out.println("evenNumbers = " + evenNumber);
+        System.out.println(" oddNumbers = " + oddNumber);
+    }
+}
