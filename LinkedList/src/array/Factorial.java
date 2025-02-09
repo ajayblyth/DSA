35ce3cdc2b6cd90a42e258d9ff9aab4e97d8abec
ajayblyth <ajayblyth@gmail.com>
2025-02-09 20:12:01 +0530

    facto/evenodd

---
+++ LinkedList/src/array/Factorial.java
@@ -0,0 +1,16 @@
+package array;
+
+public class Factorial {
+    public static void main(String[] args) {
+        int number = 5;
+        factorial(number);
+
+    }
+    public static void factorial(int num){
+        int result = 1;
+        for(int i = 1; i <= num; i ++){
+            result *= i;
+        }
+        System.out.println(result);
+    }
+}
