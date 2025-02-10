1d911701a6abdf0959a281cce4cb61a4a01752f3
ajayblyth <ajayblyth@gmail.com>
2025-02-10 13:51:43 +0530

    doubly_append/prepend

--- LinkedList/src/Doubly/DoublyLinkedList.java
+++ LinkedList/src/Doubly/DoublyLinkedList.java
@@ -3,6 +3,9 @@ package Doubly;
 public class DoublyLinkedList {
     public static void main(String[] args) {
         DoublyLinkedList myDLL = new DoublyLinkedList(1);
+        myDLL.append(2);
+        myDLL.prepend(0);
+
         myDLL.printAll();
     }
     private Node head;
@@ -30,7 +33,30 @@ Node(int value){
             System.out.println(temp.value);
             temp = temp.next;
 
-        }
-
+        }}
+    public boolean append(int value){
+       Node newNode = new Node(value);
+       if(length == 0){
+           head = newNode;
+           tail = newNode;
+       }else{
+       tail.next = newNode;
+       newNode.prev = tail;
+       tail = newNode;
+       }
+       length++;
+       return true;
+    }
+    public void prepend(int value){
+       Node newNode = new Node(value);
+       if( length == 0){
+           head = newNode;
+           tail = newNode;
+       }
+       else {
+           head.prev = newNode;
+           newNode.next = head;
+           head = newNode;
+       }length++;
     }
 }
