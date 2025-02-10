13f20a314c1674c68198a7532f3e699e74234c7b
ajayblyth <ajayblyth@gmail.com>
2025-02-10 10:49:53 +0530

    sllpractice1/append/prepend//removefirst

--- LinkedList/src/Singly/SLLPractice1.java
+++ LinkedList/src/Singly/SLLPractice1.java
@@ -1,49 +1,84 @@
-//package Singly;
-//
-//public class SLLPractice1 {
-//    public static void main(String[] args) {
-//      LinkedList myLinkedList = new LinkedList(11);
-//      myLinkedList.printAll();
-//      myLinkedList.append(22);
-//        }
-//
-//        public static class LinkedList{
-//        private Node head;
-//        private Node tail;
-//        private int length;
-//
-//        class Node {
-//            int value;
-//            Node next;
-//            public Node(int value){
-//                this.value=value;
-//            }
-//        }
-//        public LinkedList(int value){
-//            Node newNode = new Node(value);
-//            head = newNode;
-//            tail = newNode;
-//            length = 1;
-//        }
-//
-//        public void printAll(){
-//           Node temp = head;
-//           while(temp != null){
-//               System.out.println(temp.value);
-//               temp= temp.next;
-//           }
-//
-//        }
-//        public void append(int value){
-//            if( )
-//
-//        }
-//        }
-//
-//
-//}
-//
-//
-//
-//
-//
+package Singly;
+
+public class SLLPractice1 {
+    public static void main(String[] args) {
+      LinkedList myLinkedList = new LinkedList(11);
+      myLinkedList.append(22);
+      myLinkedList.prepend(10);
+      myLinkedList.removeFirst();
+        myLinkedList.printAll();
+
+    }
+
+        public static class LinkedList{
+        private Node head;
+        private Node tail;
+        private int length;
+
+        class Node {
+            int value;
+            Node next;
+            public Node(int value){
+                this.value=value;
+            }
+        }
+        public LinkedList(int value){
+            Node newNode = new Node(value);
+            head = newNode;
+            tail = newNode;
+            length = 1;
+        }
+
+        public void printAll(){
+           Node temp = head;
+           while(temp != null){
+               System.out.println(temp.value);
+               temp= temp.next;
+           }
+
+        }
+        public void append(int value){
+            Node newNode = new Node(value);
+            if(length == 0 ) {
+                head = newNode;
+                tail = newNode;
+            }
+                else{ tail.next = newNode;
+                    tail = newNode;
+
+                }
+                length++;
+
+            }
+            public void prepend(int value){
+            Node newNode = new Node(value);
+            if(length == 0){
+                head = newNode;
+                tail = newNode;
+            }
+            newNode.next = head;
+            head = newNode;
+            length ++;
+        }
+        public Node removeFirst(){
+            if (length == 0) return  null;
+            if (length == 1){
+                head = null;
+                tail = null;
+            }
+            Node temp = head;
+            head = head.next;
+            temp.next =null;
+            length --;
+
+            return temp;
+        }
+        }
+
+
+}
+
+
+
+
+
