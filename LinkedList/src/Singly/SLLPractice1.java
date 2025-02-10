e94a464a483506574a4f967178a9f676ff704013
ajayblyth <ajayblyth@gmail.com>
2025-02-10 12:11:25 +0530

    set/remove/insert

--- LinkedList/src/Singly/SLLPractice1.java
+++ LinkedList/src/Singly/SLLPractice1.java
@@ -4,8 +4,13 @@ public class SLLPractice1 {
     public static void main(String[] args) {
       LinkedList myLinkedList = new LinkedList(11);
       myLinkedList.append(22);
+      myLinkedList.append(30);
+      myLinkedList.append(40);
       myLinkedList.prepend(10);
-        System.out.println(myLinkedList.get(0).value);
+//        System.out.println(myLinkedList.get(0).value);
+        myLinkedList.set(1 , 12);
+        myLinkedList.insert(2, 44);
+//        myLinkedList.remove(2);
 //      myLinkedList.removeFirst();
 //      myLinkedList.removeLast();
         myLinkedList.printAll();
@@ -99,11 +104,42 @@ public class SLLPractice1 {
                 }return temp;
 
             }
+            public boolean set(int index, int value){
+            if(index < 0 || index >= length)return  false;
+            Node temp = get(index);
+                {if (temp !=null){
+                    temp.value = value;
+                }return false;
+                }
+            }
+            public Node remove(int index){
+            if (index < 0 || index >= length) return null;
+            if (index == 0 )removeFirst();
+            if (index == length -1) removeLast();
+            Node prev = get(index -1);
+            Node temp = prev.next;
+            prev.next = temp.next;
+            temp.next =null;
+            length --;
+            return temp;
 
-    }
+            }
+            public boolean insert(int index, int value){
+            if(index < 0 || index >= length)return false;
 
+            if (index == 0){prepend(value);
+            return true;}
 
+            if(index == length -1) {append(value);
+            return true;}
+            Node newNode = new Node(value);
+            Node temp = get (index - 1);
+            newNode.next = temp.next;
+            temp.next = newNode;
+            length++;
+            return true;
 
+    }}
 
 
 }
