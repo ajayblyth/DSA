b1488abf95f25a6c72c5cac02f967c5b2392e476
ajayblyth <ajayblyth@gmail.com>
2025-02-12 13:00:12 +0530

    Binary Search Tree

---
+++ LinkedList/src/BinarySearchTree/BinarySearchTree.java
@@ -0,0 +1,17 @@
+package BinarySearchTree;
+
+public class BinarySearchTree {
+    Node root;
+
+    class Node{
+        int value;
+        Node right;
+        Node left;
+
+        private Node (int value){
+            this.value = value;
+        }
+
+
+    }
+}
---
+++ LinkedList/src/BinarySearchTree/Main.java
@@ -0,0 +1,9 @@
+package BinarySearchTree;
+
+public class Main {
+    public static void main(String[] args) {
+        BinarySearchTree myBST = new BinarySearchTree();
+        System.out.println(" root : " + myBST.root);
+
+    }
+}
