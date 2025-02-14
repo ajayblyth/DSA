87a301e77dd8862dbf484ba003600914cb800d29
ajayblyth <ajayblyth@gmail.com>
2025-02-14 12:31:02 +0530

    set/get

--- DSA/src/HashTable/HashTable.java
+++ DSA/src/HashTable/HashTable.java
@@ -39,4 +39,28 @@ public class HashTable {
         }
         return hash;
     }
+    public void set(String key, int value){
+        int index = hash(key);
+        Node newNode = new Node(key, value);
+        if(dataMap[index] == null){
+            dataMap[index] = newNode;
+        }else{
+            Node temp = dataMap[index];
+            while(temp.next != null){
+                temp=temp.next;
+            }
+            temp.next = newNode;
+        }
+
+    }
+    public int get(String key){
+        int index = hash(key);
+        Node temp = dataMap[index];
+        while(temp != null){
+            if(temp.key == key) return temp.value;
+            temp= temp.next;
+        }
+        return 0;
+
+    }
 }
--- DSA/src/HashTable/Main.java
+++ DSA/src/HashTable/Main.java
@@ -3,6 +3,19 @@ package HashTable;
 public class Main {
     public static void main(String[] args) {
 HashTable myHashTable = new HashTable();
-myHashTable.printAll();
+myHashTable.set("nails", 1000);
+        myHashTable.set("nuts", 500);
+        myHashTable.set("bolts", 400);
+
+        myHashTable.set("tiles", 678);
+
+        myHashTable.set("screws", 843);
+
+        System.out.println(myHashTable.get("screws"));
+        System.out.println(myHashTable.get("yo"));
+
+
+
+//        myHashTable.printAll();
     }
 }
