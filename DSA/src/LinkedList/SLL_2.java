898060a88283b2c42bd8e74759be2066e81db8f2
ajayblyth <ajayblyth@gmail.com>
2025-02-19 14:31:41 +0530

    RF/get/set/insert/remove

--- DSA/src/LinkedList/SLL_2.java
+++ DSA/src/LinkedList/SLL_2.java
@@ -12,6 +12,11 @@ package LinkedList;
 
         myLinkedList.prepend(50);
         myLinkedList.removeFirst();
+        myLinkedList.removeLast();
+        System.out.println("get = " + myLinkedList.get(2).value);
+        myLinkedList.set(2, 301);
+        myLinkedList.insert(2,302);
+        System.out.println(myLinkedList.remove(3).value);
         myLinkedList.printAll();
         myLinkedList.getLength();
     }
@@ -85,10 +90,75 @@ public Node removeFirst(){
     return temp;
 
 
+}
+public Node removeLast(){
+             if(length == 0) return  null;
+             Node temp = head;
+             if(length ==1){
+                 head = null;
+                 tail= null;
+                 length--;
+                 return temp;
+             }
+             Node pre = head;
+             while(temp.next != null){
+                 pre =temp;
+                 temp = temp.next;
+             }
+             tail = pre;
+             tail.next = null;
+             length--;
+             return  temp;
 
 }
+public Boolean insert(int index, int value){
+             if(index<0 || index >length)return false;
+             if(index == 0) {
+                 prepend(value);
+                 return true;
+             }
+             if(index == length){
+                 append(value);
+                 return true;
+             }
+             Node newNode = new Node(value);
+             Node temp = get(index-1);
+   newNode.next = temp.next;
+    temp.next = newNode;
+    length++;
+             return true;
+}
+      private Boolean set(int index,int value){
+             Node temp = get(index);
+             if(temp!=null){
+                 temp.value =value;
+                 return true;
+             }return false;
 
-     }
+
+      }
+      private Node get(int index){
+           if(index<0 || index >= length)return null;
+           Node temp = head;
+           for (int i =0; i<index; i++){
+               temp = temp.next;
+           }return temp ;
+      }
+      public Node remove(int index){
+             if(index < 0 || index >=length) return null;
+             if(index == 0) return removeFirst();
+             if(index == length-1) return  removeLast();
+             Node prev = get(index-1);
+
+              Node temp = prev.next;
+              prev.next = temp.next;
+              temp.next = null;
+              length--;
+              return temp;
+
+      }
+
+  }
 
 
 }
